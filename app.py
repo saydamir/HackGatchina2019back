0a1bcca5c4c2d347254db5dd8c01b9af2801a3c0
damir <damir@MacBook-Air-damir.local>
2019-05-25 12:36:42 +0300

    Add image upload

---
+++ app.py
@@ -0,0 +1,69 @@
+from pymongo import MongoClient
+import datetime
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import hashlib
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+client = MongoClient('localhost', 27017)
+
+db = client['gatchina']
+collection = db['problems']
+
+@app.route('/post_problem', methods=['POST'])
+def post_problem():
+    content = request.get_json()
+    print(type(content))
+    post = {"author": "Mike",
+            "text": "My first blog post!",
+            "coordinate": "33.44, 55.22",
+            "date": datetime.datetime.utcnow()}
+    
+    post_id = collection.insert_one(content).inserted_id
+    return str(post_id)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = hashlib.md5(file.read()).hexdigest() + secure_filename(file.filename).rsplit('.', 1)[1].lower()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
