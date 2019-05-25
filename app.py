cf38d14ebab2cfb97088325fe1ad23fb5c9f475f
damir <damir@MacBook-Air-damir.local>
2019-05-25 13:09:58 +0300

    Change endpoints

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 import hashlib
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 
 app = Flask(__name__)
@@ -17,23 +17,32 @@ client = MongoClient('localhost', 27017)
 db = client['gatchina']
 collection = db['problems']
 
-@app.route('/post_problem', methods=['POST'])
-def post_problem():
+@app.route('/issues', methods=['POST'])
+def post_issue():
     content = request.get_json()
-    print(type(content))
-    post = {"author": "Mike",
-            "text": "My first blog post!",
-            "coordinate": "33.44, 55.22",
-            "date": datetime.datetime.utcnow()}
+    # post = {"author": "Mike",
+            # "text": "My first blog post!",
+            # "coordinate": "33.44, 55.22",
+            # "date": datetime.datetime.utcnow()}
     
     post_id = collection.insert_one(content).inserted_id
     return str(post_id)
+
+# @app.route('/issues', methods=['GET'])
+# def get_issues():
+    # cursor = collection.find({})
+    # documents = []
+    # for document in cursor:
+        # documents.append(document)
+    # 
+    # print(documents)
+    # return str(post_id)
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/images', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -47,10 +56,9 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = hashlib.md5(file.read()).hexdigest() + secure_filename(file.filename).rsplit('.', 1)[1].lower()
+            filename = hashlib.md5(file.read()).hexdigest() + "." + secure_filename(file.filename).rsplit('.', 1)[1].lower()
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return filename
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -63,7 +71,7 @@ def upload_file():
 
 from flask import send_from_directory
 
-@app.route('/uploads/<filename>')
+@app.route('/image/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
