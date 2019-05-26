b3d3ccfd2fa1e9e0ea48dc03dc12931fd9d18fdd
damir <damir@MacBook-Air-damir.local>
2019-05-26 03:18:12 +0300

    test data script

--- test_data/test_data_upload.sh
+++ test_data/test_data_upload.sh
@@ -1,3 +1,3 @@
-cd img; for i in `ls`; do curl -s -X POST -H "Content-Type: multipart/form-data" -F "file=@${i}" http://localhost:5000/images ;done ; cd ..
+cd img; for i in `ls`; do curl -s -X POST -H "Content-Type: multipart/form-data" -F "file=@${i}" $1/images ;done ; cd ..
 
-cd test_issues; for i in `ls`; do curl -s -X POST -d "@${i}" http://localhost:5000/issues ;done
+cd test_issues; for i in `ls`; do curl -s -H "Content-Type: application/json" -X POST -d "@${i}" $1/issues ;done
